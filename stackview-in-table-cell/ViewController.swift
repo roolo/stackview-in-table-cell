1b8415bda63f31b7172b3142bf420ce9b5ad1768
Mailo Světel <development@rooland.cz>
2018-10-22 13:41:15 +0200

    Initial Commit

---
+++ stackview-in-table-cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackview-in-table-cell
+//
+//  Created by Mailo Světel on 22/10/2018.
+//  Copyright © 2018 rooland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ stackview-in-table-cellTests/stackview_in_table_cellTests.swift
@@ -0,0 +1,34 @@
+//
+//  stackview_in_table_cellTests.swift
+//  stackview-in-table-cellTests
+//
+//  Created by Mailo Světel on 22/10/2018.
+//  Copyright © 2018 rooland. All rights reserved.
+//
+
+import XCTest
+@testable import stackview_in_table_cell
+
+class stackview_in_table_cellTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
